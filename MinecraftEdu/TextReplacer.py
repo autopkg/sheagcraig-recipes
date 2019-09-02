14de7b3fe05b34422631ba6f55c4793e5b8dcad1
Elliot Jordan <elliot@elliotjordan.com>
2019-09-01 17:02:12 -0700

    Standardize indentation on spaces

--- MinecraftEdu/TextReplacer.py
+++ MinecraftEdu/TextReplacer.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#	  http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -58,5 +58,5 @@ class TextReplacer(Processor):
 
 
 if __name__ == '__main__':
-	processor = TextReplacer()
-	processor.execute_shell()
+    processor = TextReplacer()
+    processor.execute_shell()
